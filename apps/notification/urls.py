c6f4ce31c7cca97f018acce27c21682942105464
Dale Stewart <stewartdale073@gmail.com>
2021-05-04 11:40:55 -0400

    Settings files

---
+++ apps/notification/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from .views import notifications
+
+urlpatterns = [
+    path('', notifications, name='notifications'),
+    
+]
---
+++ codingjobs/urls.py
@@ -0,0 +1,36 @@
+"""codingjobs URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+
+from apps.core.views import frontpage, signup
+from apps.job.views import job_detail, add_job
+from apps.userprofile.views import dashboard
+
+urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('admin/', admin.site.urls),
+    path('dashboard/', include('apps.userprofile.urls')),
+    path('notifications/', include('apps.notification.urls')),
+    path('jobs/', include('apps.job.urls')),
+
+    # path('jobs/add/', add_job, name='add_job'),
+    # path('jobs/<int:job_id>/', job_detail, name='job_detail'),
+]
